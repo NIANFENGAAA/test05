dd32224fb0a954bf3a88ff1e6e11f4539513e991
NIANFENGAAA <150124207+NIANFENGAAA@users.noreply.github.com>
2024-03-12 16:21:46 +0800

    Update Hello.java

--- src/main/java/com/zwq/Hello.java
+++ src/main/java/com/zwq/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
         System.out.println("push test!!!");
         System.out.println("push test!!!");
         System.out.println("push test!!!");
-
+        System.out.println("pull test!!!");
+        System.out.println("pull test!!!");
     }
 }
