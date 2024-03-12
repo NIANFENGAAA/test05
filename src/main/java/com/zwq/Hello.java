fbc476bd906880374b5cd69b3efaa84578086d22
NIANFENG <1323965112@qq.com>
2024-03-12 15:27:09 +0800

    third commit

--- src/main/java/com/zwq/Hello.java
+++ src/main/java/com/zwq/Hello.java
@@ -3,5 +3,7 @@ package com.zwq;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("version2");
+        System.out.println("333");
     }
 }
